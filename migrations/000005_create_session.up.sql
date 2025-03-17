ae844292451de7211339628841e73e17d343eb51
JackieLi565 <li.jackie565@gmail.com>
2025-03-17 00:01:01 -0400

    fix: update sessions table

--- migrations/000005_create_session.up.sql
+++ migrations/000005_create_session.up.sql
@@ -1,8 +1,8 @@
 create table sessions (
+    id uuid primary key default gen_random_uuid(),
     user_id uuid not null references users(id) on delete cascade,
     date_added timestamp not null default now(),
-    date_expires timestamp not null,
-    constraint sessions_id primary key (user_id, date_added)
+    date_expires timestamp not null
 );
 
 create index date_expires_idx on sessions (date_expires);
